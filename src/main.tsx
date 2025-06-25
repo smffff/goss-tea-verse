d441987c5b0670f8aec8f76c9ded6c0cad073b25
lady <stephanie@taskbytask.net>
2025-06-24 19:39:27 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,4 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Simple test component
-const TestApp = () => {
-  return (
-    <div style={{ 
-      padding: '20px', 
-      backgroundColor: '#1a0d26', 
-      color: 'white', 
-      minHeight: '100vh',
-      fontSize: '24px'
-    }}>
-      <h1>CTea Newsroom Test</h1>
-      <p>If you can see this, React is working!</p>
-      <p>Background should be dark purple</p>
-    </div>
-  );
-};
-
-// Try the main App component
 createRoot(document.getElementById("root")!).render(<App />);
